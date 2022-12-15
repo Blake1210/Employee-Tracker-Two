33a5a069f41bb59b879a9d1c4d8b14cec1d5f4be
Blake <blakepetersont@gmail.com>
2022-12-15 13:25:46 -0700

    making progress on the index.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,20 @@ DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    NAME VARCHAR(40) NOT NULL
+    NAME VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(40) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
 ),
 
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
 )
