5e0b7bac3fb2293138c1629706ee33fc0ce3bde8
Blake <blakepetersont@gmail.com>
2022-12-15 11:34:00 -0700

    made the db and schema sql

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+USE company;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+),
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+)
