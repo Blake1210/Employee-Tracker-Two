dcc111e6ac57f60c99020f5a334a361feb9e11a6
Blake <blakepetersont@gmail.com>
2022-12-15 12:33:25 -0700

    added the data for the tables

---
+++ db/data.sql
@@ -0,0 +1,29 @@
+USE company;
+INSERT INTO DEPARTMENTS (NAME)
+VALUES
+("Marketing"),
+("Operations"),
+("Human Resources"),
+("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+("Head of Marketing", 120000, 1),
+("Marketing Associate", 70000, 1),
+("Head of Operations", 110000, 2),
+("Supply Chain Manager", 8000, 2),
+("Recruiter", 75000, 3),
+("Sales Manager", 15000, 4),
+("Account Manager", 120000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+("Jon", "Snow", 1),
+("Samwell", "Tarley", 2),
+("Tyrion", "Lannister", 3),
+("Robb", "Stark", 4),
+("Daemon", "Targaryen", 5),
+("Jorah", "Mormont", 6),
+("Brienne", "Tarth", 7),
+("Arya", "Stark", 4),
+("Rhaenyra", "Targaryen", 5);
